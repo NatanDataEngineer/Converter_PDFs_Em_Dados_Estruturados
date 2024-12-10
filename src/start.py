fc1856c454a6e6a16e4a5938fd1afbaace786a78
NathanVittorino <natan18silva96@gmail.com>
2024-12-10 19:36:49 -0300

    O cabeçalho do PDF foi tratado

--- src/start.py
+++ src/start.py
@@ -5,6 +5,7 @@ import logging
 from unidecode import unidecode
 
 class PDFTableExtractor:
+
     def __init__(self, file_name, configs):
         self.path = os.path.abspath(f"src/files/pdf/{configs["name"].lower()}/{file_name}.pdf")
         self.csv_path = os.path.abspath(f"src/files/csv")
@@ -13,24 +14,31 @@ class PDFTableExtractor:
 
     def start():
         pass
-    def get_table_data(self, t_area, t_columns):
+
+    def get_table_data(self, t_area, t_columns, fix):
         tables = camelot.read_pdf(
             self.path,
             flavor=self.configs["flavor"],
             table_areas = t_area,
             columns = t_columns,
             strip_text = self.configs["strip_text"],
-            pages = self.configs["page"],
+            page = self.configs["page"],
             password= self.configs["password"]
         )
+
         
 
     def save_csv():
         pass
     def add_infos():
         pass
-    def fix_header():
-        pass
+
+    @staticmethod
+    def fix_header(df):
+        df.columns = df.iloc[0] # Uses the first row as column names
+        df = df.drop(0) # Removes the first row
+        df = df.drop(df.columns[0], axis=1) # Removes the first column
+
     def sanitize_column_names():
         pass
     def send_to_db():
